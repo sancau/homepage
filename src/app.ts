eced972414dac77435571f42a76dfd9be09e481b
Alexander Tatchin <alexander.tatchin@gmail.com>
2016-07-06 16:52:13 +0300

    typescript/typings works

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,25 @@ import * as express from "express";
 
 var app = express();
 
-var lyrics = 'ATATA'
-
 app.get('/', function(req:express.Request, res:express.Response){
-  res.send(`<h1>${lyrics}</h1>`);
+  res.send(`<h1>Test App</h1>`);
 });
 
 app.listen(3000, function(){
-  console.log('Listening on port 3000...');
+  console.log('Test app is listening on port 3000...');
 });
 
+/////////////////////////////////////////////////////////////////
+
+var anotherApp = express();
+
+anotherApp
+.get(
+  '/', 
+  (req: express.Request, res: express.Response) =>
+   res.send('Hello World!')
+);
+
+let port = 3140
+anotherApp.listen(port, () => 
+  console.log(`Another app is listening on port ${port}...`));
