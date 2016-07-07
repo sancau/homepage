9ca29ba94a8a665df9a93c694d248cf11f230135
Alexander Tatchin <alexander.tatchin@gmail.com>
2016-07-07 17:50:04 +0300

    Updated builder

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@
 import * as express from 'express'
 
 var app = express().set('view engine', 'jade');
-
 app.use(express.static(__dirname + '/public'));
 app.get('/', (req: express.Request, res: express.Response) => res.render('index', {title: 'Index'}));
 app.get('/about', (req: express.Request, res: express.Response) => res.render('about', {title: 'About'}));
